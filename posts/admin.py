a1ff8fee822bc9455b4c5bd1ce85f482d96c59ca
Егор <gok6@inbox.ru>
2021-05-21 18:11:45 +0300

    Initial commit

---
+++ posts/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+
+from .models import Post, Comment
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("pk", "text", "pub_date", "author")
+    search_fields = ("text",)
+    list_filter = ("pub_date",)
+    empty_value_display = '-пусто-'
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("pk", "author", "post", "text", "created")
+    search_fields = ("text",)
+    list_filter = ("created",)
+    empty_value_display = '-пусто-'
+
+
+# при регистрации модели Post источником конфигурации для неё назначаем класс PostAdmin
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
